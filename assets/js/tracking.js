e8148bf8bb669dec540348bb3d74940efdd4e105
Thies Mueller <github@td00.de>
2023-09-06 23:16:36 +0200

    switched the logic around

--- assets/js/tracking.js
+++ assets/js/tracking.js
@@ -3,9 +3,9 @@
 			var exclusionState = window.localStorage.plausible_ignore == "true"
 
 			if (exclusionState) {
-				document.getElementById("plausible_button").innerHTML = 'Tracking activated'
-			} else {
 				document.getElementById("plausible_button").innerHTML = 'Tracking deactivated'
+			} else {
+				document.getElementById("plausible_button").innerHTML = 'Tracking activated'
 			}
 		});
 
@@ -14,10 +14,10 @@
 
 			if (exclusionState) {
 				delete window.localStorage.plausible_ignore
-				document.getElementById("plausible_button").innerHTML = 'Tracking deactivated'
+				document.getElementById("plausible_button").innerHTML = 'Tracking activated'
 			} else {
 				window.localStorage.plausible_ignore = "true"
-				document.getElementById("plausible_button").innerHTML = 'Trackin activated'
+				document.getElementById("plausible_button").innerHTML = 'Tracking deactivated'
 			}
 		}
 	
